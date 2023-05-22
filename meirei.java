be2d3a2c612b070bb770d4ef99dd201bf366a1f9
io <kapi.name.pooh@gmail.com>
2023-05-22 14:09:33 +0900

    テンプレファイル追加

---
+++ meirei.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class meirei{
+	public static void main(String[] args){
+		System.out.println(引数);//引数を出力して改行
+		System.out.print(引数);//引数を出力(改行なし)
+		System.out.printf("%s(%d)こんにちは%n","山田",23);//1未満の乱数を生成
+		double n =Math.random();//1未満の乱数を生成
+		int n =new Random().nextInt(自然数);//0その自然数未満の値をランダムに生成
+		int n = Math.max(a,b);//aとbを比較して大きい方の値を返す
+		int n = Math.min(a,b);//aとbを比較して小さい方の値を返す
+		int n = Integer.parseInt(str);//strの文字列をintに変換
+		String str=new Scanner(System.in).nextLine(); //キーボードからの入力を１行まるっと受け取る
+		String str=new Scanner(System.in).next(); //キーボードからの入力から一つの単語を受け取る
+		int n = new Sc
+			double w = new java.util.Scanner(System.in).nextDouble();
+		double h = Double.parsedouble("152.3");
+		String line = new Scanner(System.in).nextLine();
+		//配列の内容を表示
+		int[] arr = {3,5,7};
+		System.out.println(Arrays.toString(arr));
+	}
+}
