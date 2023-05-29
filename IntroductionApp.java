12f901d25f09e40e598e3d1c1787e2c32539fd35
io <kapi.name.pooh@gmail.com>
2023-05-29 11:46:16 +0900

    自己紹介アプリソート機能追加

--- IntroductionApp.java
+++ IntroductionApp.java
@@ -36,11 +36,12 @@ public class IntroductionApp{
 		showStatus(status,params);
 		System.out.println("***作成成功!***");
 		System.out.printf("私は%sの%s、%sです。%n",classes[cls],races[race],name);
-		System.out.println("能力値(" + sumStatus(status)+")");
+		System.out.println("能力値(" + sumStatus(status)+")を高い順に並べると");
+		sortStatus(status,params);
 		for(int i =0;i<status.length;i++){
 			System.out.printf("%s:%d%n",params[i],status[i]);
 		}
-			System.out.println("です。今後ともよろしく...");
+		System.out.println("です。今後ともよろしく...");
 	}
 	static int calcSeed(String name){
 		//seed(種)
@@ -93,5 +94,19 @@ public class IntroductionApp{
 		//最後の,削除
 		str = str.substring(0,str.length()-1);
 		return str;
+	}
+	static void sortStatus(int[] status,String[] Params){
+		for(int i =0;i<status.length-1;i++){
+			for(int j=i+1;j<status.length;j++){
+				if(status[i] < status[j]){
+					int t1 = status[i];
+					status[i]=status[j];
+					status[j]=t1;
+					String t2 = Params[i];
+					Params[i] = Params[j];
+					Params[j] = t2;
+				}
+			}
+		}
 	}
 }
